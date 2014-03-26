a8ae5d8be4e28e6583e3df0d0f6183c84103257d
Jimi-Gib <jweber000@gmail.com>
2014-03-26 15:10:04 -0700

    xslt tester appears to work

--- app/controllers/listener_controller.rb
+++ app/controllers/listener_controller.rb
@@ -1,18 +1,31 @@
 class ListenerController < ApplicationController
 	def receive_email
-		
 		@params = params
+
+		# if params[:additional_recipients_only]
+      # @additional_recipients_only = true
+    # else
+      @additional_recipients_only = false
+    # end
+
 		@email = Email.new(
+			:subject => @params.to_s,
+			:recipients => "shhtmltest@gmail.com",
 			# :text => params["text"],
-			:markup => params["html"]
+			:markup => params["html"],
 			# :to => params["to"],
 			# :from => params["from"],
 			# :subject => params["subject"]
+			:subject => "test"
 			)
-		@email.subject = @params.to_s
-
+		if @email.markup.nil?
+			@email.markup = "The body was empty for some reason"
+		end
+		# @email.subject = @params.to_s
+		# @email.recipients = "shhtmltest@gmail.com"
 		respond_to do |format|
 		  if @email.save
+		  	@email.send_emails_via_ses(current_user , @additional_recipients_only)
 		    format.html { redirect_to @email, notice: "XSLT Email successfully created" }
 		  else
 		    format.html { render action: "new" }
