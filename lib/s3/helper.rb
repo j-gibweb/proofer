910b1fc35520e569c28952a763071900603be197
James Weber <jweber@rmwarmbpjweber.us.responsys.com>
2014-04-18 13:07:29 -0700

    promotional_spec does s3 upload test

--- lib/s3/helper.rb
+++ lib/s3/helper.rb
@@ -1,7 +1,50 @@
 module S3
   class Helper
+    
     def self.unique_file_name(object)
       "#{object.class}_#{object.created_at}".gsub!(/:/,"").gsub!(/ /,"_").gsub!(/-/,"_").downcase
     end
+    
+    def self.unzip(zip, unzip_dir, remove_after = false)
+      Zip::ZipFile.open(zip) do |zip_file|
+        zip_file.each do |f|
+          f_path=File.join(unzip_dir, f.name)
+          FileUtils.mkdir_p(File.dirname(f_path))
+          zip_file.extract(f, f_path) unless File.exist?(f_path)
+        end
+      end
+      FileUtils.rm(zip) if remove_after
+      # puts Dir[File.dirname(zip)+"/**/*"]
+    end
+
+    def self.push_assets_to_s3 object , bucket 
+      threads = []
+      S3::Helper.inner_file_paths(object).each do |f|
+        next if File.directory?(f) 
+        threads << Thread.new{
+          file_name = f.sub( File.dirname(f) , "" )
+          if [".jpg" , ".gif" , ".png"].any? { |ext| file_name.include?(ext) } 
+            key = "#{S3::Helper.unique_file_name(object)}/#{S3::Helper.images_dir_name(f)}#{file_name}"
+          elsif file_name.include? ".htm" 
+            key = "#{S3::Helper.unique_file_name(object)}#{file_name}"
+          end
+          file = FOG_STORAGE.directories.get(bucket).files.create( 
+            :key => key , 
+            :body => File.open(f), 
+            :public => true 
+            ) 
+        }
+      end
+      threads.each(&:join)
+    end
+
+    def self.images_dir_name object
+      "#{File.dirname(object).split("/").last}"
+    end
+
+    def self.inner_file_paths object
+      Dir["#{File.dirname(object.folder.path)}/**/*"] 
+    end
+
   end
 end
--- spec/models/promotional_spec.rb
+++ spec/models/promotional_spec.rb
@@ -8,13 +8,18 @@ describe Promotional do
   end
 
   it "generates unique s3 bucket name from s3 helper class" do
-    expect(@helper = S3::Helper.new).to be_instance_of(Helper)
     @promotional = Promotional.create(:html => "<body><p>test test</p></body>")
     expect(S3::Helper.unique_file_name(@promotional)).to eq("#{@promotional.class}_#{@promotional.created_at}".gsub!(/:/,"").gsub!(/ /,"_").gsub!(/-/,"_").downcase)   
   end
 
-  it "unzips an html email and passes the code to html_parser" do
+  it "unzips an html email .zip and uploads everything to s3" do
     @promotional = Promotional.create(:folder => File.new("#{Rails.root}/test_upload_files/perfect_case.zip"))
+    S3::Helper.unzip(@promotional.folder.path , File.dirname(@promotional.folder.path) , true)
+    S3::Helper.push_assets_to_s3(@promotional , "proofer-stage")
   end
+
+  it "converts image file paths in HTML to the paths of the s3 bucket" do
+
+  end 
 
 end
